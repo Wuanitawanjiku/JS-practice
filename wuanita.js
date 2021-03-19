7594864c52e9d4324a3248076fc34a375c91402d
Wuanitawanjiku <wanjikuwuanita@gmail.com>
2021-03-19 16:43:48 +0300

    first  commit

---
+++ wuanita.js
@@ -0,0 +1,11 @@
+function name(){
+    var a = "Shadya"
+    var b = "Pamela"
+
+    function identity(){
+    var c =a +" " + "and" +" "+ b +" "+ "are friends";
+    console.log(c);
+}
+identity();
+}
+name()
